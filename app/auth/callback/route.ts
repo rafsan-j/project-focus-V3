325ab41cd84cec65a4e819dca89b47f672af0a36
Rafsan <rafsanj2972@outlook.com>
2026-03-28 18:02:59 +0600

    Implement GET route for authentication callback

---
+++ app/auth/callback/route.ts
@@ -0,0 +1,27 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
+import { NextResponse, type NextRequest } from 'next/server'
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url)
+  const code = requestUrl.searchParams.get('code')
+
+  if (code) {
+    const response = NextResponse.redirect(requestUrl.origin + '/')
+    const supabase = createServerClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      {
+        cookies: {
+          getAll() { return request.cookies.getAll() },
+          setAll(cookiesToSet: { name: string; value: string; options: CookieOptions }[]) {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              response.cookies.set(name, value, options))
+          },
+        },
+      }
+    )
+    await supabase.auth.exchangeCodeForSession(code)
+    return response
+  }
+  return NextResponse.redirect(requestUrl.origin + '/login')
+}
